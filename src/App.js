99b70bf22f48b2ba65762984bbe0cd058eb002e8
Leonardo Gonçalves <souoleo@gmail.com>
2022-03-21 22:41:28 -0300

    fetch API icanhazdadjoke

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { useState } from "react";
 import "./styles.css";
 
-const tellJoke = () => {
-  console.log("joke");
-};
-
 export default function App() {
-  return <button onClick={tellJoke}>Tell me a joke!</button>;
+  const [joke, setJoke] = useState();
+
+  const onTellJoke = () => {
+    fetch("https://icanhazdadjoke.com/", {
+      method: "GET",
+      headers: {
+        Accept: "application/json"
+      }
+    })
+      .then((response) => response.json())
+      .then((json) => setJoke(json.joke));
+  };
+  return (
+    <div>
+      <button onClick={onTellJoke}>Tell me a joke!</button>
+      <p>{joke}</p>
+    </div>
+  );
 }
